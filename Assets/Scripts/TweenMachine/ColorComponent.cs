b98b4ec30f3cc9a00f982c4f65e445a69be6a342
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-04-19 11:31:43 +0200

    Added scale and color lerp components

---
+++ Assets/Scripts/TweenMachine/ColorComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorComponent : VectorLerpComponent
+{
+    Renderer rend;
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    protected override void ApplyLerp(Vector3 result)
+    {
+        rend.material.color = new Color(result.x, result.y, result.z);
+    }
+}
---
+++ Assets/Scripts/TweenMachine/ScaleComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleComponent : VectorLerpComponent
+{
+    protected override void ApplyLerp(Vector3 result)
+    {
+        this.transform.localScale = _startVector + result;
+    }
+}
